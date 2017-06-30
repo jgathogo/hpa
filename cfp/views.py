ab48288feef71e7bd4c8255a1f2f63fb63d7052f
James Gathogo <james@gathogo.co.ke>
2017-06-30 19:42:40 +0300

    Let's pause here first and go through the MDN Tutorial, it will save us so much time

--- cfp/views.py
+++ cfp/views.py
@@ -1,8 +1,9 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Cfp, Donor, Theme, Zone
+from .forms import EditCfPForm
 
 
 def cfp_home(request):
@@ -20,3 +21,22 @@ class IndexView(generic.ListView):
 class CfPDetailView(generic.DetailView):
     model = Cfp
     template_name = 'cfp/cfp_detail.html'
+
+
+def edit_cfp(request, pk):
+    cfp_inst = get_object_or_404(Cfp, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+        # Create a form instance and populate it with data from the request
+        # (binding):
+        form = EditCfPForm(request.POST)
+
+        # Check if the form is valid
+        if form.is_valid():
+            cfp_inst.due_back = form.cleaned_data['cfp_title']
+            cfp_inst.save()
+
+            #redirect to a new URL:
+            # Read MDN tutorial then come back, 30 June 2017
+            
