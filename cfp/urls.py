aa59e9aa21ab424055900019a14aa7b1a66e32eb
James Gathogo <james@gathogo.co.ke>
2017-06-30 18:40:25 +0300

    Revert master to bbasic, workable state

--- cfp/urls.py
+++ cfp/urls.py
@@ -6,6 +6,5 @@ app_name = 'cfp'
 urlpatterns = [
     url(r'^$', views.cfp_home),
     url(r'all-calls-for-proposals/$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.CfPDetailView.as_view(), name='cfp_detail'),
-    url(r'^(?P<pk>[0-9]+)/edit-cfp/$', views.edit_cfp, name='edit_cfp'),
+    url(r'^(?P<pk>[0-9]+)/$', views.CfPDetailView.as_view(), name='cfp_detail'),    
 ]
